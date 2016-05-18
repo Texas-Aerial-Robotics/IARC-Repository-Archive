2c737806cbd159bb6ccf892f1c60844856e90480
Eric Johnson <eric.emj.johnson@gmail.com>
2016-05-18 03:29:36 -0500

    finished physics
    the simulation for the robots is finished

--- robotPositionSim.m
+++ robotPositionSim.m
@@ -1,43 +1,76 @@
 %the purpose of this script is to calculate the robots positions overtime
-% last modified 5/17/2016 2:54pm by Eric Johnson
+% last modified 5/18/2016 3:28am by Eric Johnson
 clc; clear all
-R = 4; %radius for troll bots
+RT = 5; %radius for troll bots
 t = 0; %time variable
 h = .5; %step size for time 
 v = .25; %m/s
 rVector =0;% distance travled
-direction = -1; % simulates 180 turns
+direction = -1*ones(1,10); % simulates 180 turns
+R=1; %starting radius for normal bots
 
-% loops at h interval to ten mins 
+%loops 10 times
+%   the body of the for loop set the intial postiotions of the bots
+for i=1:10
+    dtheta0 = 2*pi / 10;
+    x0Bot(i) = R*cos(dtheta0*i);
+    y0Bot(i) = R*sin(dtheta0*i);
+    yBots(1,i) =y0Bot(i);
+    xBots(1,i) =x0Bot(i);
+end
+% %loops 4 times for the 4 troll bots
+% %   the body of the loop calculates the initial positions of the troll bots
+% for i=1:4
+%   theta0 = (2*pi/4) * i;
+%   yt1((1/h)*t + 1,i) = RT*sin(theta0);
+%   xt1((1/h)*t + 1,i) = RT*cos(theta0);
+% end
+
+% loops at h interval to ten mins
 %   body of loop calculates the positions of bots
-x0 = R*sin(pi/4);
-y0 = R*cos(pi/4);
-yb1(1) =y0;
-xb1(1) =x0;
 for t=0:h:600
-    %troll bot 1
-    theta = (v*t)/R;
-    yt1((1/h)*t + 1) = R*sin(theta);
-    xt1((1/h)*t + 1) = R*cos(theta);
-    
-    %bot 1
-    thetaBot1 = pi/4;
-    
-    rVector = v*h ;
-    yb1((1/h)*t + 2) = direction*rVector*sin(thetaBot1) + yb1((1/h)*t+1);
-    xb1((1/h)*t + 2) = direction*rVector*cos(thetaBot1) + xb1((1/h)*t+1);
     if mod(t,20) == 0
-       direction = direction * -1;
+        disp('time')
+        direction = direction * -1;
     end
-    deltaT1B1y = yb1((1/h)*t + 2)-yt1((1/h)*t + 1);
-    deltaT1B1x = xb1((1/h)*t + 2)-xt1((1/h)*t + 1);
-    colitionRT1B1 = sqrt((deltaT1B1y^2) + (deltaT1B1x^2));
-    if colitionRT1B1 < .68
-       direction = direction * -1;
-       disp('colition')
-       t
+    %troll bots positions
+    for n=1:4
+        theta = (v*t)/RT;
+        theta0 = (2*pi/4);
+        yt1((1/h)*t + 2,n) = RT*sin(theta + theta0*n);
+        xt1((1/h)*t + 2,n) = RT*cos(theta+ theta0*n);
     end
     
+    
+    %bot positions
+    %%%
+    
+    %loops ten times 
+    %   body of loop calculates the position for the ten bots at the
+    %   specific t value
+    for k=1:10
+        thetaBot = dtheta0*k;
+        rVector = v*h ;
+        yBots((1/h)*t + 2,k) = direction(k)*rVector*sin(thetaBot) + yBots((1/h)*t+1,k);
+        xBots((1/h)*t + 2,k) = direction(k)*rVector*cos(thetaBot) + xBots((1/h)*t+1,k);
+    end
+    
+    for ii=1:4
+        %loops ten for ten bots
+        %   body of the loop cheks for collisions between bots
+        for i=1:10
+            deltaRy(i) = yBots((1/h)*t + 2, i)-yt1((1/h)*t + 1,ii);
+            deltaRx(i) = xBots((1/h)*t + 2, i)-xt1((1/h)*t + 1,ii);
+            collition(i) = sqrt((deltaRy(i)^2) + (deltaRx(i)^2));
+            if collition(i) < .68
+                direction(i) = direction(i) * -1;
+                disp('collition')
+                t
+                i
+                ii
+            end
+        end
+    end
 end
 
 
@@ -45,8 +78,12 @@ figure(1)
 axis([-10,10,-10,10]);
 hold on
 for i=1:length(xt1)
-    plot(xt1(i),yt1(i),'ro','markersize', 6)
-    plot(xb1(i),yb1(i),'bo','markersize', 6)
+    for k=1:4
+        plot(xt1(i,k),yt1(i,k),'ro','markersize', 6) 
+    end
+    for n=1:10
+        plot(xBots(i,n),yBots(i,n),'bo','markersize', 6)
+    end
     hold on
     drawnow
 end
