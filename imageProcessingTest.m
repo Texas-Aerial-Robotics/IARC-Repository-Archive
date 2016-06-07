339e0561b49251a79a942598a7aa2754ef8bc746
Eric Johnson <eric.emj.johnson@gmail.com>
2016-06-07 17:00:56 -0500

    fixed smoothing filter

--- imageProcessingTest.m
+++ imageProcessingTest.m
@@ -12,7 +12,7 @@ info = imfinfo('FiftyGray.jpg')
 % adds noise 
 for p=1:250
    for q=1:374
-     g(q,p) = g(q,p) + 20*randn(1);  
+     g(q,p) = g(q,p) + 10*randn(1);  
    end
 end
 subplot(1,3,2), imshow(g)
@@ -21,15 +21,15 @@ subplot(1,3,2), imshow(g)
 %smoothing using average filter 
 f = ones(3);
 subM = zeros(3);
-for p=2:249
-    for q=2:373
+for p=3:248
+    for q=3:372
         sum = 0;
-        for x=-1:1
-            for y=-1:1
-                sum=g(q+y,p+x);
+        for x=-2:2
+            for y=-2:2
+                sum=double(g(q+y,p+x))+sum;
             end
         end
-        gFiltered(q,p) = (1/9)*sum;
+        gFiltered(q,p) = round((1/25)*sum);
     end
 end
-subplot(1,3,3), imshow(gFiltered)
+subplot(1,3,3), imshow(mat2gray(gFiltered))
