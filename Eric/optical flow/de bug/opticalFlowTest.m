2d16fd3b35c9bee53371ade816f59a058111c047
Markloveland <markloveland@utexas.edu>
2016-09-12 17:03:19 -0500

    optical flow

--- Eric/optical flow/de bug/opticalFlowTest.m
+++ Eric/optical flow/de bug/opticalFlowTest.m
@@ -5,5 +5,12 @@ im2 = imread('I2.jpg');
 
 figure(1); imshow(mat2gray(im1))
 figure(2); imshow(mat2gray(im2))
-[u, v] = lucasKanade(im1, im2)
+tic
+[u, v] = lucasKanade(im1, im2);
+toc
+uimage=mat2gray(u);
+vimage=mat2gray(v);
+
+figure(3); imshow(uimage)
+figure(4); imshow(vimage)
 
