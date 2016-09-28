383f0dc638520c871b4f6b97eea6af4b592cb1f3
Eric Johnson <eric.emj.johnson@gmail.com>
2016-09-28 14:50:34 -0500

    added comments to the programs from meeting 2

--- Meeting Programs/createGuassian.m
+++ Meeting Programs/createGuassian.m
@@ -1,13 +1,25 @@
+%the purpose of this user defined function is to generate a guassian mask
+%   the inputs for this function is the dimenstion ***MUST BE AN ODD
+%   INTEGER***
+%   the outputs for tis function is the guassian mask
+%   created 9/21/16
 function guassMask = createGuassian(dimension)
 
-
-%define average mask
+%create zeros matrix size of mask
 guassMask = zeros(dimension);
-sigma=1;
+%define sigma 
+sigma=(dimension-1)/3;
+
+%apply guassian function using mask positions as points
+%   NOTE: we want the mask to be symetric so we shift the origin to the
+%   center of the matrix
 for row=1:dimension
     for col=1:dimension
+        %shift origin 
         x=col-((dimension+1)/2);
         y=row-((dimension+1)/2);
+        
+        %apply guassian function 
         guassMask(row,col)=(1/sqrt(2*pi*sigma))*exp(-(x^2+y^2)/(2*sigma^2));
         
     end
--- Meeting Programs/gaussfilterexample.m
+++ Meeting Programs/gaussfilterexample.m
@@ -1,13 +1,22 @@
-%gauss filter example
+%The purpose of this script is to show an example of applying a guassian
+%   filter
+%   created 11/21/16
+clc; clear all 
+close all 
+%inport image 
 Image=rgb2gray(imread('BMWi8.jpg'));
-figure(1)
-imshow(Image)
+figure(1), imshow(Image)
+
+%change image to a matrix type
 Matrix=double(Image);
 
+%generate guassian mask
 dimension = 25;
 guassMask = createGuassian(dimension);
 
+%apply mask to the image 
 I = convolution1(Matrix, guassMask);
+
+%change matrix back to image type and display 
 Image2=mat2gray(I);
-figure(2)
-imshow(Image2)
+figure(2), imshow(Image2)
