49bd2f01fcce44723f4c85950ce7e1a392ae3578
Eric Johnson <eric.emj.johnson@gmail.com>
2017-02-04 21:35:33 -0600

    mullti tracking, and updated camera mount

---
+++ Eric/CV C++/multiTracking/multiTracking/Fruit.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <string>
+#include <opencv\cv.hpp>
+using namespace std;
+using namespace cv;
+class Fruit
+{
+public:
+	Fruit();
+	Fruit(string name);
+	~Fruit();
+	int getXPos();
+	int getYPos();
+	void setXPos(int x);
+	void setYPos(int y);
+
+	void setHSVMin(Scalar min);
+	void setHSVMax(Scalar max);
+
+	string getType() { return type; }
+	void setType(string t) { type = t; }
+
+	Scalar getHSVMin();
+	Scalar getHSVMax();
+
+	Scalar getColor() { return color; }
+	void setColor(Scalar c) { color = c; }
+
+private:
+	int xPos, yPos;
+	string type;
+    Scalar HSVMin, HSVMax;
+	Scalar color;
+};
+
