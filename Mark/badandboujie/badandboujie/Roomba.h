3b52671744fbe666927be5a23bf8e1cd046df54d
Markloveland <markloveland@utexas.edu>
2017-02-23 22:06:39 -0600

    Update

---
+++ Mark/badandboujie/badandboujie/Roomba.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <string>
+#include <opencv/cv.hpp>
+#include <opencv2\highgui\highgui.hpp>
+using namespace cv;
+using namespace std;
+class Roomba
+{
+public:
+	Roomba();
+	~Roomba();
+	int getXPos();
+	void setXPos(int x);
+
+	int getYPos();
+	void setYPos(int y);
+
+
+
+	cv::Scalar getHSVmin();
+	cv::Scalar getHSVmax();
+
+	void setHSVmin(Scalar min);
+	void setHSVmax(Scalar min);
+
+private:
+
+	int xPos, yPos;
+	string type;
+	cv::Scalar HSVmin, HSVmax;
+};
+
